e8947e27f6666c89fb795db49f85cd977e0f3da7
Yaji Tersoo <tyaji247@gmail.com>
2025-03-01 17:26:31 +0100

    Initial commit

--- staff_tracker/settings.py
+++ staff_tracker/settings.py
@@ -130,8 +130,12 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Ensure you have a 'static' folder in your Django project
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Use WhiteNoise for serving static files
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
 
 
 # ✅ Media Files (Uploaded Files)
